fc543296a3bf87ceb116fcdff83a19535cc346ce
Levi Evans <levi.samuel.evans@cern.ch>
2024-02-18 22:49:35 +0000

    format scheduler with black

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -1,13 +1,26 @@
 import torch
 from torch.optim.lr_scheduler import _LRScheduler
+import math
 
-'''
+"""
 Heavily inspired from implementation by Chris Scheulen found here: 
 https://gitlab.cern.ch/utils_chscheul/naf_utils/neural-network-for-tthbb/-/blob/main/utils/lr_schedules.py?ref_type=heads
-'''
+"""
+
 
 class CosineRampUpDownLR(_LRScheduler):
-    def __init__(self, optimizer, lr_init=1e-8, lr_max=1e-5, lr_final=1e-7, burn_in=10, ramp_up=10, plateau=20, ramp_down=100, last_epoch=-1):
+    def __init__(
+        self,
+        optimizer,
+        lr_init=1e-8,
+        lr_max=1e-5,
+        lr_final=1e-7,
+        burn_in=10,
+        ramp_up=10,
+        plateau=20,
+        ramp_down=100,
+        last_epoch=-1,
+    ):
         self.lr_init = lr_init
         self.lr_max = lr_max
         self.lr_final = lr_final
@@ -28,7 +41,10 @@ class CosineRampUpDownLR(_LRScheduler):
 
         if epoch < self.ramp_up:
             fraction = 0.5 * (1.0 - math.cos(math.pi * (epoch + 1) / self.ramp_up))
-            return [(self.lr_max - self.lr_init) * fraction + self.lr_init for _ in self.base_lrs]
+            return [
+                (self.lr_max - self.lr_init) * fraction + self.lr_init
+                for _ in self.base_lrs
+            ]
 
         epoch -= self.ramp_up
 
@@ -39,6 +55,9 @@ class CosineRampUpDownLR(_LRScheduler):
 
         if epoch < self.ramp_down:
             fraction = 0.5 * (1.0 + math.cos(math.pi * (epoch + 1) / self.ramp_down))
-            return [(self.lr_max - self.lr_final) * fraction + self.lr_final for _ in self.base_lrs]
+            return [
+                (self.lr_max - self.lr_final) * fraction + self.lr_final
+                for _ in self.base_lrs
+            ]
 
         return [self.lr_final for _ in self.base_lrs]
