1bf215c6c3c77675f20e6225259ab34bd0dec8c7
Levi Evans <levi.samuel.evans@cern.ch>
2024-06-04 02:35:34 +0200

    fix: improve notification of how the script works

--- enviroment/activate_env.sh
+++ enviroment/activate_env.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-# directory where Miniconda is installed, default is $HOME/miniconda3
-CONDA_INSTALL_DIR="${1:-$HOME/miniconda3}"
+# check if a path was provided
+if [ $# -eq 0 ]; then
+  echo "No path provided. Using the default home directory ($HOME/miniconda3)."
+  echo "To specify a different path, run the script as: ./activate_env <path/to/miniconda3>"
+  echo
+fi
 
-# name of environment, default is MLenv
+CONDA_INSTALL_DIR="${1:-$HOME/miniconda3}"
 ENV_NAME="${2:-MLenv}"
 
-# does the Conda installation exist?
+# is conda even installed?
 if [ -d "$CONDA_INSTALL_DIR" ]; then
   echo "Miniconda found at $CONDA_INSTALL_DIR"
 else
-  echo "Miniconda is not installed at $CONDA_INSTALL_DIR. Please run create_miniconda.sh first, or point to correct installation."
+  echo "Miniconda is not installed at $CONDA_INSTALL_DIR. Please run create_miniconda.sh first, or point to the correct installation."
   exit 1
 fi
 
-# init conda
+# initialise conda
 source "$CONDA_INSTALL_DIR/etc/profile.d/conda.sh"
 conda init
 
-# does the environment exist?
+# does the enviroment exist?
 if conda env list | grep -qw "$ENV_NAME"; then
   echo "Environment '$ENV_NAME' exists."
 else
@@ -26,8 +30,6 @@ else
   exit 1
 fi
 
-# activate
 echo "Activating environment '$ENV_NAME'..."
 conda activate "$ENV_NAME"
-
 echo "Setup complete. Virtual environment '$ENV_NAME' is ready to use! Happy ML'ing! ;D"
